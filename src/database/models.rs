b805d139998647c7404818241295f4b7cc7533f8
Andrew Plaza <aplaza@liquidthink.net>
2019-09-21 21:36:36 +0200

    comments

--- src/database/models.rs
+++ src/database/models.rs
@@ -8,61 +8,99 @@ use chrono::NaiveDateTime;
 // TODO: Make generic
 type DB = diesel::pg::Pg;
 
-
+/// The table for accounts
 #[derive(Queryable, PartialEq, Debug)]
 pub struct Blocks {
+    /// hash of the previous block
     parent_hash: H256,
+    /// Hash of this block
     hash: H256,
+    /// The block number
     block: usize,
+    /// root of the state trie
     state_root: H256,
+    /// root of the extrinsics trie
     extrinsics_root: H256,
+    /// timestamp
     time: Option<NaiveDateTime>
 }
 
+/// Inherants (not signed) extrinsics
 #[derive(Queryable, PartialEq, Debug)]
 pub struct Inherants {
+    /// PostgreSQL Generated ID/Primary Key (No meaning within substrate/chains)
     id: usize,
+    /// Hash of the block this inherant was created in, foreign key
     hash: H256,
+    /// Block number of the block this inherant was created in
     block: usize,
+    /// Module the inherant called
     module: String,
+    /// Call within the module inherant used
     call: String,
+    /// Was the call succesful?
     success: bool,
+    /// Index of the inherant within a block
     in_index: usize,
 }
 
+/// Signed Extrinsics (More like traditional transactions)
 #[derive(Queryable, PartialEq, Debug)]
 pub struct SignedExtrinsics {
+    /// Hash of the transaction, primary key
     transaction_hash: H256,
+    /// the block this transaction was created in
     block: usize,
+    /// the account that originated this transaction
     from_addr: H256,
+    /// the account that is receiving this transaction, if any
     to_addr: Option<H256>,
+    /// The call this transaction is using
     call: String,
+    /// was the transaction succesful?
     success: bool,
+    /// nonce of the transaction
     nonce: usize,
+    /// Index of the transaction within the block it originated in
     tx_index: usize,
+    /// signature of the transaction
     signature: H512
 }
 
+/// Accounts  on thechain
 #[derive(Queryable, PartialEq, Debug)]
 pub struct Accounts {
+    /// Address of the account (So far only ed/sr) Primary key
     address: H256,
+    /// Free balance of the account
     free_balance: usize,
+    /// Reserved balanced
     reserved_balance: usize,
+    /// Index of the account within the block it originated in
     account_index: Vec<u8>,
+    /// nonce of the account
     nonce: usize,
+    /// the block that this account was created in, Foreign key
     create_hash: H256,
+    /// Block number that this account was created in
     created: usize,
+    /// Block that this account was last updated
     updated: usize,
+    /// whether this account is active
     active: bool
 }
 
 
+/// NewType for custom Queryable trait on Substrates H256 type
 #[derive(PartialEq, Debug)]
 pub struct H256(SubstrateH256);
+
+/// NewType for custom Queryable trait on Substrates H512 type
 #[derive(PartialEq, Debug)]
 pub struct H512(SubstrateH512);
 
 impl H512 {
+    /// Get the H512 back into substrate type
     fn into_inner(self) -> SubstrateH512 {
         self.0
     }
@@ -92,6 +130,7 @@ where
 }
 
 impl H256 {
+    /// Get the H256 back into substrate type
     fn into_inner(self) -> SubstrateH256 {
         self.0
     }
