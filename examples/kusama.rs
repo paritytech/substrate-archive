a6762bea75b2b785ccb0d0fa77e37b15ead9c3d2
Andrew Plaza <aplaza@liquidthink.net>
2020-04-08 19:54:44 +0200

    rustfmt

--- examples/kusama.rs
+++ examples/kusama.rs
@@ -1,25 +1,31 @@
 use desub::decoder::Decoder;
 use desub_extras::polkadot::PolkadotTypes;
-use subxt::{KusamaRuntime, system::System};
-use substrate_archive::Archive;
 use futures::FutureExt;
+use substrate_archive::Archive;
+use subxt::{system::System, KusamaRuntime};
 
 pub fn main() {
     substrate_archive::init_logger(log::LevelFilter::Info, log::LevelFilter::Debug);
     let types = PolkadotTypes::new().unwrap();
     let decoder = Decoder::new(types, "kusama");
     log::info!("Creating client");
-    let client = async_std::task::block_on(subxt::ClientBuilder::<KusamaRuntime>::new().set_url("ws://127.0.0.1:9944").build()).unwrap();
-    
-    let block = async_std::task::block_on(client.block::<<KusamaRuntime as System>::Hash>(None)).unwrap();
+    let client = async_std::task::block_on(
+        subxt::ClientBuilder::<KusamaRuntime>::new()
+            .set_url("ws://127.0.0.1:9944")
+            .build(),
+    )
+    .unwrap();
+
+    let block =
+        async_std::task::block_on(client.block::<<KusamaRuntime as System>::Hash>(None)).unwrap();
     println!("{:?}", block);
 
-    log::info!("Instantiating archive..."); 
+    log::info!("Instantiating archive...");
     let archive = Archive::new(decoder, client).unwrap();
     log::info!("Beginning to crawl for info");
     let (data, blocks, blocks_batch, batch_handler) = archive.parts().unwrap();
     async_std::task::spawn(blocks);
-    async_std::task::spawn(async { 
+    async_std::task::spawn(async {
         match blocks_batch.await {
             Ok(_) => (),
             Err(e) => {
@@ -31,16 +37,13 @@ pub fn main() {
     let batch_handler = async {
         match batch_handler.await {
             Ok(_) => (),
-            Err(e) => { 
+            Err(e) => {
                 log::error!("{:?}", e);
                 panic!("internal archive error");
             }
         }
     };
-    let handlers = futures::future::join(batch_handler, data); 
+    let handlers = futures::future::join(batch_handler, data);
 
-    
     async_std::task::block_on(handlers);
 }
-
-
