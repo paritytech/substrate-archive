c2352f483ecd5801e15d33baab9bb7901f665e26
Andrew Plaza <andrew.plaza@parity.io>
2020-09-11 14:52:06 +0200

    rustfmt and more example refactoring

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,40 +1,29 @@
-// Copyright 2017-2019 Parity Technologies (UK) Ltd.
-// This file is part of substrate-archive.
-
-// substrate-archive is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// substrate-archive is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with substrate-archive.  If not, see <http://www.gnu.org/licenses/>.
-
 //! A simple example
+
 use polkadot_service::{kusama_runtime::RuntimeApi, Block, KusamaExecutor};
 use substrate_archive::{Archive, ArchiveBuilder};
 
 pub fn main() {
     substrate_archive::init_logger(log::LevelFilter::Info, log::LevelFilter::Info);
-    
+
     // get spec/runtime from node library
     let spec = polkadot_service::chain_spec::kusama_config().unwrap();
 
+    // export DATABASE_URL="postgres://postgres:123@localhost:5432/kusama_db"
     let db_url = std::env::var("DATABASE_URL").unwrap();
+    // export CHAIN_DATA_DB="/home/insipx/.local/share/polkadot/chains/ksmcc3/db"
+    let chain_backend_url = std::env::var("CHAIN_DATA_DB").unwrap();
+
     let mut archive = ArchiveBuilder::<Block, RuntimeApi, KusamaExecutor>::default()
         .block_workers(2)
         .wasm_pages(512)
         .cache_size(128)
         .pg_url(db_url)
+        .chain_data_db(chain_backend_url)
         .chain_spec(Box::new(spec))
         .build()
         .unwrap();
 
-    archive.drive();
-    archive.block_until_stopped();
+    archive.drive().unwrap();
+    futures::executor::block_on(archive.block_until_stopped());
 }
-
