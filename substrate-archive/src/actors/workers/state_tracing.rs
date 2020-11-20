f97b8fdc9bacc15144016b999199b59f486f36cf
Andrew Plaza <andrew.plaza@parity.io>
2020-11-20 16:02:19 +0100

    custom substrate for testing

--- substrate-archive/src/actors/workers/state_tracing.rs
+++ substrate-archive/src/actors/workers/state_tracing.rs
@@ -14,12 +14,14 @@
 // You should have received a copy of the GNU General Public License
 // along with substrate-archive.  If not, see <http://www.gnu.org/licenses/>.
 
+use sc_tracing::{SpanDatum, TraceEvent, TraceHandler};
 use substrate_archive_common::Result;
 use tracing::{
 	event::Event,
 	span::{Attributes, Id, Record},
 	Level, Metadata, Subscriber,
 };
+// use tracing_subscriber::{layer::SubscriberExt, EnvFilter};
 use xtra::prelude::*;
 
 #[derive(Clone)]
@@ -32,10 +34,20 @@ impl ArchiveTraceHandler {
 		Self { addr }
 	}
 }
+/*
+impl TraceHandler for ArchiveTraceHandler {
+	fn handle_span(&self, sd: SpanDatum) {
+		self.addr.do_send(SpanMessage(sd)).unwrap();
+	}
 
+	fn handle_event(&self, ev: TraceEvent) {
+		self.addr.do_send(EventMessage(ev)).unwrap();
+	}
+}
+*/
 impl Subscriber for ArchiveTraceHandler {
 	fn enabled(&self, metadata: &Metadata<'_>) -> bool {
-		log::info!("{}", metadata.target());
+		println!("{}", metadata.target());
 		true
 	}
 
@@ -43,7 +55,9 @@ impl Subscriber for ArchiveTraceHandler {
 		let meta = span.metadata();
 		match meta.target() {
 			"sp_io::hashing" | "sp_io::allocator" | "sp_io::storage" => {}
-			_ => log::info!("{}", meta.target()),
+			_ => {
+				println!("[{}]:[{}]::{:?}: {:?}", meta.target(), meta.name(), meta.module_path(), span.values());
+			}
 		}
 		Id::from_u64(1)
 	}
@@ -57,7 +71,7 @@ impl Subscriber for ArchiveTraceHandler {
 	}
 
 	fn event(&self, event: &Event<'_>) {
-		log::info!("EVENT {:?}", event);
+		println!("EVENT {:?}", event);
 	}
 
 	fn enter(&self, span: &Id) {
@@ -82,15 +96,16 @@ impl TracingActor {
 #[async_trait::async_trait]
 impl Actor for TracingActor {
 	async fn started(&mut self, ctx: &mut Context<Self>) {
-		log::info!("State Tracing Started");
+		println!("State Tracing Started");
 		let addr = ctx.address().expect("Actor just started");
 		let handler = ArchiveTraceHandler::new(addr.clone());
 		log::debug!("Trace Targets [{}]", self.targets.as_str());
+		// let layer = ProfilingLayer::new_with_handler(Box::new(handler), self.targets.as_str());
+		// let subscriber = tracing_subscriber::fmt().with_max_level(Level::TRACE).finish();
 		tracing::subscriber::set_global_default(handler).unwrap();
 	}
 }
 
-/* TODO: Uncomment when wasm_tracing issues resolved
 #[derive(Debug)]
 struct SpanMessage(SpanDatum);
 
@@ -122,7 +137,7 @@ impl Handler<EventMessage> for TracingActor {
 		log::info!("Event: {:?}", msg);
 	}
 }
-*/
+
 #[async_trait::async_trait]
 impl Handler<super::Die> for TracingActor {
 	async fn handle(&mut self, _: super::Die, ctx: &mut Context<Self>) -> Result<()> {
