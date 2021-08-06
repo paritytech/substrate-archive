9ae66bef5d5ae2f11a8d5e0ff7550dc7d59498b8
Andrew Plaza <andrew.plaza@parity.io>
2021-08-06 11:24:44 -0400

    update docker file

---
+++ scripts/docker_postgres_init.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE kusama
+  WITH
+  OWNER = postgres
+  ENCODING = 'UTF8'
+  LC_COLLATE = 'en_US.utf8'
+  LC_CTYPE = 'en_US.utf8'
+  TABLESPACE = pg_default
+  CONNECTION LIMIT = -1;
+CREATE DATABASE test
+  WITH 
+  OWNER = postgres
+  ENCODING = 'UTF8'
+  LC_COLLATE = 'en_US.utf8'
+  LC_CTYPE = 'en_US.utf8'
+  TABLESPACE = pg_default
+  CONNECTION LIMIT = -1;
