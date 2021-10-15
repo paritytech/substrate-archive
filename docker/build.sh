442685164eeefc5e558e16c2c7077829c76fc5ac
Andrew Plaza <andrew.plaza@parity.io>
2021-10-15 12:27:34 +0200

    un-exclude bin/

--- docker/build.sh
+++ docker/build.sh
@@ -8,7 +8,7 @@ PROJECT_ROOT=`git rev-parse --show-toplevel`
 cd $PROJECT_ROOT
 
 # Find the current version from Cargo.toml
-VERSION=`grep "^version" ./Cargo.toml | egrep -o "([0-9\.]+)"`
+VERSION=`grep "^version" ./bin/polkadot-archive/Cargo.toml | egrep -o "([0-9\.]+)"`
 GITUSER=parity
 GITREPO=substrate-archive
 
