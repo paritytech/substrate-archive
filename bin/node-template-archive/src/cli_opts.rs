519bdbbbd73e68a818f0a8f31fd12da3d1220cb8
Andrew Plaza <andrew.plaza@parity.io>
2021-10-18 17:29:34 +0200

    fix node-template-archive compile

--- bin/node-template-archive/src/cli_opts.rs
+++ bin/node-template-archive/src/cli_opts.rs
@@ -14,12 +14,15 @@
 // You should have received a copy of the GNU General Public License
 // along with substrate-archive.  If not, see <http://www.gnu.org/licenses/>.
 
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, borrow::Cow};
 
 use anyhow::Result;
 use structopt::StructOpt;
 
 use substrate_archive::ArchiveConfig;
+
+/// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
+pub type ChainSpec = sc_chain_spec::GenericChainSpec<node_template_runtime::GenesisConfig>;
 
 #[derive(Clone, StructOpt)]
 #[structopt(author, about)]
@@ -27,18 +30,26 @@ pub struct CliOpts {
 	/// Sets a custom config file
 	#[structopt(short = "c", long, name = "FILE")]
 	pub config: Option<PathBuf>,
-	/// Sets spec for chain to run in (dev/local).
+	/// Sets spec for chain from a JSON file. Runs in `dev` mode by default.
 	#[structopt(short = "s", long = "spec", name = "CHAIN", parse(from_str = parse_chain_spec))]
-	pub chain_spec: node_template::chain_spec::ChainSpec,
+	pub chain_spec: ChainSpec,
 }
 
-fn parse_chain_spec(spec: &str) -> node_template::chain_spec::ChainSpec {
-	let spec = match spec {
-		"dev" => node_template::chain_spec::development_config(),
-		"" | "local" => node_template::chain_spec::local_testnet_config(),
-		path => node_template::chain_spec::ChainSpec::from_json_file(PathBuf::from(path)),
-	};
-	spec.expect("Chain spec could not be loaded")
+impl Default for CliOpts {
+	fn default() -> Self {
+
+		let file = include_bytes!("./dev.json");
+		let file: Cow<'static, [u8]> = Cow::Borrowed(file);
+		let chain_spec: ChainSpec = ChainSpec::from_json_bytes(file).expect("Default ChainSpec `dev` could not be loaded");
+		Self {
+			config: None,
+			chain_spec,
+		}
+	}
+}
+
+fn parse_chain_spec(path: &str) -> ChainSpec {
+	ChainSpec::from_json_file(PathBuf::from(path)).expect("Chain spec could not be loaded")
 }
 
 impl CliOpts {
