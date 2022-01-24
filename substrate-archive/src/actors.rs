3f5da5470caa8c35a14b3280359aa35f4b2ab80d
Andrew Plaza <aplaza@liquidthink.net>
2022-01-24 14:55:01 -0500

    document sa-work-queue

--- substrate-archive/src/actors.rs
+++ substrate-archive/src/actors.rs
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with substrate-archive.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Main entrypoint for substrate-archive. `init` will start all actors and begin indexing the
+//! Main entrypoint for substrate-archive. `init` will start the actor loop and begin indexing the
 //! chain defined with the passed-in Client and URL.
 
 mod workers;
@@ -199,8 +199,8 @@ where
 		Ok(Actors { storage, blocks, metadata, db, extrinsics })
 	}
 
-	// Run a future that sends actors a signal to progress once the previous
-	// messages have been processed.
+	/// Run a future that sends actors a signal to progress once the previous
+	/// messages have been processed.
 	async fn tick_interval(&self) -> Result<()> {
 		// messages that only need to be sent once
 		self.blocks.send(ReIndex).await?;
--- work-queue/sa-work-queue/src/lib.rs
+++ work-queue/sa-work-queue/src/lib.rs
@@ -1,3 +1,69 @@
+//! A library that utilizes a RabbitMQ queue to execute background tasks
+//!
+//!### Usage
+//!
+//! The core of this work-queue is the `#[background_job]` macro which implements
+//! the [`Job`] trait, allowing for the enqueuing & performing of jobs.
+//!
+//! ```no_run
+//! #[sa_work_queue::background_job]
+//! fn do_some_blocking_work(name: String) -> Result<(), sa_work_queue::PerformError> {
+//!    std::thread::sleep(std::time::Duration::from_millis(150));
+//!    println!("Job {} finished!", name);
+//!    Ok(())
+//! }
+//! let runner = sa_work_queue::Runner::builder((), "Im a job")
+//!    .register_job::<do_some_blocking_work::Job>()
+//!    .build()
+//!    .unwrap();
+//! runner.run_pending_tasks().unwrap();
+//! ```
+//!
+//! Each background job may have a `Environment` attached that is passed into every job.
+//! This could be useful to store dynamic information such as database urls, database connections, backend clients, etc.
+//!
+//!
+//!```no_run
+//!
+//!pub struct MyEnvironment {
+//!    db_url: String
+//!}
+//!#[sa_work_queue::background_job]
+//!fn do_some_blocking_work(env: &MyEnvironment, name: String) -> Result<(), sa_work_queue::PerformError> {
+//!   std::thread::sleep(std::time::Duration::from_millis(150));
+//!   println!("Job {} finished with database url {}!", name, env.db_url);
+//!   Ok(())
+//!}
+//!
+//!let env = MyEnvironment {
+//!   db_url: "postgres://postgres@localhost:5432/postgres".to_string()
+//!};
+//!
+//!let runner = sa_work_queue::Runner::builder(env, "Im a job")
+//!   .register_job::<do_some_blocking_work::Job>()
+//!   .build()
+//!   .unwrap();
+//!runner.run_pending_tasks().unwrap();
+//!```
+//!
+//! ### RabbitMQ Requirements
+//!
+//! This library requires a RabbitMQ service running on the host. The address is defined by [`Builder::new`].
+//! The URL along with RabbitMQ prefetch and queue name may be configured.
+//! function
+//! ```no_run
+//!Runner::builder((), "amqp://localhost:5672")
+//!    .queue_name("my-work-queue")
+//!    .num_threads(2)
+//!    .prefetch(200)
+//!    .timeout(Duration::from_secs(5))
+//!    .build()
+//!    .unwrap()?;
+//! ```
+//!
+//!
+
+
 #![deny(unused, dead_code)]
 #![forbid(unsafe_code)]
 #[doc(hidden)]
