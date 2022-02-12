6583d69a405d31e6889d189e98a8d77e93797846
m0ssc0de <hi.paul.q@gmail.com>
2022-02-12 14:40:05 +0800

    nix

---
+++ shell.nix
@@ -0,0 +1,23 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  channel = pkgs.rustChannelOf {
+     channel = "nightly";
+  };
+  rust = (channel.rust.override {
+    targets = [ "wasm32-unknown-unknown" ];
+  });
+in
+  with pkgs;
+  stdenv.mkDerivation {
+    name = "rust-dev-env";
+    buildInputs = [
+      rust
+      llvmPackages.libclang
+      llvmPackages.libcxxClang
+      clang
+      pkg-config openssl
+    ];
+    LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
+    BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${llvmPackages.libclang.lib}/lib/clang/${lib.getVersion clang}/include";
+}
