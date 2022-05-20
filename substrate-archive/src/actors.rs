62ed59b870414fbc3950bc15a2021013a33efb57
Joey <joey.xie@deeper.network>
2022-05-20 22:33:41 +0800

    decrease prefetch count to 100 (#472)
    to avoid ack timeout 1800s

--- substrate-archive/src/actors.rs
+++ substrate-archive/src/actors.rs
@@ -374,7 +374,7 @@ where
 			.register_job::<crate::tasks::execute_block::Job<Block, Runtime, Client, Db>>()
 			.num_threads(self.config.runtime.block_workers)
 			.queue_name(queue)
-			.prefetch(5000)
+			.prefetch(100)
 			// times out if tasks don't start execution on the threadpool within timeout.
 			.timeout(Duration::from_secs(self.config.control.task_timeout))
 			.build()?;
